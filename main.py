78d8d1bcdc1428f851120c90a926f1618c9d54c7
Elay <88445864+IgresRF@users.noreply.github.com>
2022-10-11 15:42:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord import Embed, File
 from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
 
-token = "ODc2NzUzNTM5MTkwMzgyNjIy.YRoqXQ.6Stw9nUoD7qnPPwNvKtqnw9jVTc"
+token = ""
 
 client = commands.Bot(command_prefix='+')
 client.remove_command('help')
